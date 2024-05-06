ada1e207af16fee9af99ae409e8a68f602f7d076
Mohamed Ashik <73638633+ashik12102@users.noreply.github.com>
2024-05-06 20:49:25 +0200

    filled info

--- monthly_expense.py
+++ monthly_expense.py
@@ -41,13 +41,11 @@ salary_arrived()
 # write a function to add bonus to the bank balance. take the ammount from user. can you think of any edge case?
 def add_bonus():
     # your code here
-    bonus = 2000
     global bank_balance
+    bonus = int(input("Enter the bonus amount received: "))
     bank_balance += bonus
 
     print("bank balance after bonus: ", bank_balance)
-
-#diobe ´jklfhjk
 add_bonus()
 
 
--- test.py
+++ test.py
@@ -2,14 +2,26 @@ import datetime
 
 bank_balance = 6000
 salary = 3000
+salary_drawn_count = 1
 
 def salary_arrived():
     # your code here
     global bank_balance
+    global salary_drawn_count
     if datetime.datetime.now().day ==6:
         bank_balance += salary
+        salary_drawn_count+=1
 
     print("bank balance: ", bank_balance)
 
 
 salary_arrived()
+
+def add_bonus():
+    # your code here
+    global bank_balance
+    bonus = int(input("Enter the bonus amount received: "))
+    bank_balance += bonus
+
+    print("bank balance after bonus: ", bank_balance)
+add_bonus()
